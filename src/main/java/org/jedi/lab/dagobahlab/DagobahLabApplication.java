41930504f302ac4678ad8053fc31ec2e3f2a017e
jinho-yoo-jack <jhy7342@gmail.com>
2025-09-04 22:35:37 +0900

    INIT

---
+++ src/main/java/org/jedi/lab/dagobahlab/DagobahLabApplication.java
@@ -0,0 +1,13 @@
+package org.jedi.lab.dagobahlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagobahLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DagobahLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/jedi/lab/dagobahlab/DagobahLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jedi.lab.dagobahlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DagobahLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
